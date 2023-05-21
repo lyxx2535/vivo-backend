9989d02ca94de54c519cd77f28acee05552333b0
Chuchen Deng <201250206@smail.nju.edu.cn>
2023-05-22 07:45:14 +0800

    fix: add filter

--- src/main/java/com/example/vivo_backend/configuration/CorsConfig.java
+++ src/main/java/com/example/vivo_backend/configuration/CorsConfig.java
@@ -5,22 +5,36 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    private CorsConfiguration buildConfig() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod("*");
-        corsConfiguration.setAllowCredentials(true);
-        return corsConfiguration;
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+//            .allowedOrigins("*")
+                .allowedOriginPatterns("*")// 设置允许跨域请求的域名
+                .allowedHeaders("*")// 设置允许的请求头
+                .allowCredentials(true)// 是否允许证书
+                .allowedMethods("*")// 允许的方法
+                .maxAge(3600);// 跨域允许时间
     }
 
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig());
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config); // CORS 配置对所有接口都有效
         return new CorsFilter(source);
     }
+
+
 }
+
+
