e526f35d90f895b1368402ae0197acaef01826a5
liu yuexiao <201250215@smail.nju.edu.cn>
2023-05-22 08:44:07 +0800

    style: sonarqube

--- src/main/java/com/example/vivo_backend/utils/OssUtil.java
+++ src/main/java/com/example/vivo_backend/utils/OssUtil.java
@@ -5,7 +5,6 @@ import com.aliyun.oss.model.ObjectMetadata;
 import com.aliyun.oss.model.PutObjectResult;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
